3924e602a333b954e9a69f83bb050cffc7a855fa
QuirrelForU <symmlux@gmail.com>
2024-05-19 17:04:20 +0700

    Apply headless mode for webdriver

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 
 @pytest.fixture(scope="session")
 def driver() -> WebDriver:
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
